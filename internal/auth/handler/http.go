a9433ad69f15bbc1a1461351ac7264e0ac560565
vovanbravin <viva.brovin@gmail.com>
2026-05-24 23:47:06 +0300

    fix golang-lint

--- internal/auth/handler/http.go
+++ internal/auth/handler/http.go
@@ -19,7 +19,7 @@ func NewAuthHandler(authService *service.AuthService) *AuthHandler {
 func SetError(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(map[string]string{
+	_ = json.NewEncoder(w).Encode(map[string]string{
 		"message": message,
 	})
 }
